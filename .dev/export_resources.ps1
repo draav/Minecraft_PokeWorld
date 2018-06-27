54d9f67890088834add2baaf78019952cfbbcbd8
Nicholas Devlin <devlin.nicholasj@gmail.com>
2018-06-26 23:30:37 -0400

    need to use .zip file extension, also ignore more stuff in artifact

--- .dev/export_resources.ps1
+++ .dev/export_resources.ps1
@@ -1,4 +1,4 @@
 # Nicholas Devlin
 # Archive resources pack ignoring any hidden directories
 
-7z a -tzip -xr'!.*' -x!resources resources. $PSScriptRoot\..\
+7z a -tzip -xr'!.*' -x'!resources' -x'!README.md' -x'!LICENSE' resources $PSScriptRoot\..\
