cc1ad1f31f994d1ccc1b061e0f13f11a7856fa2b
Nicholas Devlin <devlin.nicholasj@gmail.com>
2018-07-03 22:18:32 -0400

    make export resources parameters easier to use

--- .dev/export_resources.ps1
+++ .dev/export_resources.ps1
@@ -2,9 +2,9 @@
 # Archive resources pack ignoring any hidden directories
 
 param (
-    [string]$world_save_path = "C:\Users\drava\AppData\Roaming\.minecraft\resourcepacks",
+    [string]$path = "C:\Users\drava\AppData\Roaming\.minecraft\resourcepacks",
     [string]$name = "Minecraft_PokeWorld"
 )
 
-Write-Output $world_save_path
-7z a -tzip -xr'!.*' -x'!resources.zip' -x'!README.md' -x'!LICENSE' $world_save_path\$name $PSScriptRoot\..\
+Write-Output $path\$name
+7z a -tzip -xr'!.*' -x'!resources.zip' -x'!README.md' -x'!LICENSE' $path\$name $PSScriptRoot\..\
