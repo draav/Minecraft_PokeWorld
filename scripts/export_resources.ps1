6711a514a221c699d32c7ba9bae5234698c59dc1
Nicholas Devlin <devlin.nicholasj@gmail.com>
2019-04-02 16:58:20 -0400

    add usage directions for export resources script

--- scripts/export_resources.ps1
+++ scripts/export_resources.ps1
@@ -1,13 +1,16 @@
 # Nicholas Devlin
 # Export resources pack folder to Minecraft world save
 
+# usage: 
+# powershell export_resources.ps1 -resource_target "C:\Users\drava\AppData\Roaming\.minecraft_worldedit\saves\PokeWorld_1_12\resources"
+
 param (
     [string]$resource_source = "$PSScriptRoot\..\resourcepack\*",
     [string]$resource_target = "$env:APPDATA\.minecraft\saves\PokeWorld\resources",
     [string]$data_source = "$PSScriptRoot\..\datapack\*",
     [string]$data_target = "$env:APPDATA\.minecraft\saves\PokeWorld\datapacks\pokeworld"
 )
-
+C:\Users\drava\AppData\Roaming\.minecraft_worldedit\saves\PokeWorld_1_12
 Write-Output "$resource_source ==> $resource_target"
 Write-Output "$data_source ==> $data_target"
 7z a -tzip $resource_target $resource_source
